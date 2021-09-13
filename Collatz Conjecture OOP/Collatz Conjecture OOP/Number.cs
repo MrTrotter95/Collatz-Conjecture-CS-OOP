0c7d401602251ba3cbfef6b507bdc20ca73f8f94
MrTrotter95 <86136855+MrTrotter95@users.noreply.github.com>
2021-09-13 20:41:34 +1200

    Initial Upload

---
+++ Collatz Conjecture OOP/Collatz Conjecture OOP/Number.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collatz_Conjecture_OOP
+{
+    class Number
+    {
+
+        // Constructor - To create an object, and to store the value entered into MyNumber.
+        // This way allows us to adjust the variable MyNumber safely and doesn't expose it.
+        public Number(int number)
+        {
+            MyNumber = number;
+        }
+
+
+        // to get and set the value that was passed through.
+        public int MyNumber { get; set; }
+
+
+
+
+
+        // The Method to run the calculation and display the results!
+        public void DisplayResult()
+        {
+            while(MyNumber > 1)
+            {
+                if (MyNumber % 2 == 0)
+                {
+                    MyNumber = MyNumber / 2;
+                    Console.WriteLine(MyNumber);
+                } 
+                else
+                {
+                    MyNumber = MyNumber * 3 + 1;
+                    Console.WriteLine(MyNumber);
+                }
+            }
+        }
+    }
+}
---
+++ Collatz Conjecture OOP/Collatz Conjecture OOP/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collatz_Conjecture_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*The Collatz Conjecture in OOP.
+            The Collatz Connjecture is a simple math problem that still hasn't been solved.
+            The conjecture is that if you pick any number, and follow the two simple rules 
+            you will always reach a 4,2,1 loop.
+            So, if you're up for trying to see if this conjecture is false,
+             you'll have to start at 300 Quintillion. */
+
+
+            // the rules are simple. pick any number. If that number is even then / 2. If that number is Odd then * 3 + 1.
+            // to run the calculation, simply create a new object of type Number. and enter in any number you like.
+            Number first = new Number(100);
+
+            // to see the calculation happen. call the DisplayResult Method.
+            first.DisplayResult();
+            Console.Read();
+        }
+    }
+}
